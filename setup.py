0e2c4ae46fc188445a4428f0a031d1e25129cd60
yamachi <yamachig@users.noreply.github.com>
2017-12-27 22:39:25 +0900

    setup.pyを修正

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
 setup(
-    name='Sphinx csv-list',
+    name='sphinx_csv_list',
     version='0.0.1',
     description='Sphinx extension to render CSV as HTML list',
+    url='https://github.com/yamachig/sphinx_csv_list',
     author='yamachi',
     license='MIT',
     packages=['sphinx_csv_list'],
